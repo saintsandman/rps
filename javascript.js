83b0333d602a395b342a866bc27cc2a9f03b8c38
saintsandman <thesaintofsand@gmail.com>
2024-01-21 21:50:31 -0500

    Add check for winning game

--- javascript.js
+++ javascript.js
@@ -7,10 +7,17 @@ function getComputerChoice() {
   
 }
 
-let score = 0
+let score = 0;
 
 function winRound() {
   return score = ++score;
+}
+
+function winCheck() {
+  if (score == 3) {
+  return console.log("You win the game!");
+  }
+    
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -22,18 +29,22 @@ function playRound(playerSelection, computerSelection) {
     return "You Win! Rock beats Scissors!";
   } else if (playerSelection == "rock" && computerSelection == "paper") {
     return "You Lose! Paper beats Rock!";
-} else if (playerSelection == "paper" && computerSelection == "rock") {
+
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
     winRound();
     return "You Win! Paper beats Rock!";
-} else if (playerSelection == "paper" && computerSelection == "scissors") {
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
     return "You Lose! Scissors beats Paper!";
-} else if (playerSelection == "scissors" && computerSelection == "paper") {
+ 
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
     winRound();
     return "You Win! Scissors beats Paper!";
-} else if (playerSelection == "scissors" && computerSelection == "rock") {
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
     return "You Lose! Rock beats Paper!";
+
   } else if (playerSelection == computerSelection) {
     return "It's a tie! Try again!";
+
   } else {
     return "Invalid. Please enter 'Rock', 'Paper', or 'Scissors' :3";
   }
@@ -49,12 +60,17 @@ function game() {
  
  playerSelection = prompt("ROUND 3: Please Enter 'Rock', 'Paper', or 'Scissors'!");
  console.log(playRound(playerSelection));
+ winCheck();
  
  playerSelection = prompt("ROUND 4: Please Enter 'Rock', 'Paper', or 'Scissors'!");
  console.log(playRound(playerSelection));
+ winCheck();
  
  playerSelection = prompt("ROUND 5: Please Enter 'Rock', 'Paper', or 'Scissors'!");
  console.log(playRound(playerSelection));
+ winCheck();
+ 
+
 }
 
 
